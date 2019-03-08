c266c21fd28b2edf0748b2bd9a121199e260e7e9
Roberto Hernandez <info.redpaz@gmail.com>
2019-03-08 14:35:03 -0600

    Improving square style with Material-UI

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,122 +1,121 @@
-import React,{Component} from "react";
-import {Square} from "./Square";
-import './Board.css';
+import React, { Component } from "react";
+import Square from "./Square";
+import "./Board.css";
 
+class Board extends Component {
+  constructor(props) {
+    super(props);
+    //Initial state
+    this.state = {
+      boardSize: 3,
+      squares: Array(9).fill(null), //board size
+      player1IsNext: true, //By default start up player 1 (x)
+      step: 0
+    };
+  }
 
-class Board extends Component{
-    constructor(props){
-        super(props);
-     //Initial state
-     this.state = {
-         boardSize:3,
-         squares:Array(9).fill(null), //board size
-         player1IsNext : true, //By default start up player 1 (x)
-         step:0
-     }
+  /**
+   * It trigger when user click a square
+   * @param {int} index
+   */
+  handleClick(index) {
+    const squares = this.state.squares.slice();
+    //Check if a square is already fulled and also if someone has won the game
+    if (squares[index] || this.computesWinner(squares)) {
+      return;
     }
+    squares[index] = this.whoIsNext();
+    this.setState({
+      squares,
+      player1IsNext: !this.state.player1IsNext,
+      step: this.state.step + 1
+    });
+  }
 
-    /**
-     * It trigger when user click a square
-     * @param {int} index 
-     */
-    handleClick(index){
-        const squares = this.state.squares.slice();
-        //Check if a square is already  and also if someone has won the game 
-        if(squares[index] || this.computesWinner(squares) ){
-            return;
-        }
-        squares[index] = this.whoIsNext();
-        this.setState({
-            squares,
-            player1IsNext:!this.state.player1IsNext,
-            step: this.state.step + 1
-        })
+  //Knowing player's turn
+  whoIsNext() {
+    return this.state.player1IsNext ? "X" : "O";
+  }
 
+  /**
+   * Render each square
+   * @param {int} index
+   */
+  drawSquare(index) {
+    return (
+      <Square
+        value={this.state.squares[index]}
+        handleClick={() => this.handleClick(index)}
+      />
+    );
+  }
+  /**
+   * Calculate who is the winner,based on
+   * the odds (board size)
+   * @param {Array} squares
+   */
+  computesWinner(squares) {
+    const odds = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    //Traverse our odds array
+    for (let i = 0; i < odds.length; i++) {
+      //destructuring the current value, we are getting the n  index there
+      const [a, b, c] = odds[i];
+      //Validate
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
     }
+    return null;
+  }
 
-    //Knowing player's turn
-    whoIsNext(){
-        return this.state.player1IsNext ? 'X' : 'O';
+  render() {
+    const winner = this.computesWinner(this.state.squares);
+    let headerState;
+    const player = this.whoIsNext();
+    if (winner) {
+      headerState = `Player   ${winner}  won!`;
+    } else {
+      if (this.state.step === this.state.squares.length) {
+        headerState = `Its a draw!`;
+      } else {
+        headerState = `Its your turn: ${player}`;
+      }
     }
-
-   /**
-    * Render each square
-    * @param {int} index 
-    */
-    drawSquare(index){
-        return(
-            <Square 
-                value={this.state.squares[index]}
-                handleClick={() => this.handleClick(index)}
-            />
-        )
-    }
-    /**
-     * Calculate who is the winner,based on 
-     * the odds (board size)
-     * @param {Array} squares 
-     */
-    computesWinner(squares){
-        const odds = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6]
-        ];
-        //Traverse our odds array
-        for (let i = 0; i < odds.length; i++) {
-            //destructuring the current value, we are getting the n  index there
-            const [a,b,c] = odds[i];
-            //Validate
-            if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-                return squares[a];
-            }
-        }
-        return null;
-    }
-
-    render(){
-        const winner = this.computesWinner(this.state.squares);
-        let headerState;
-        const player = this.whoIsNext();
-        if(winner){            
-            headerState = `Player   ${winner}  won!`;
-        }else{
-            if(this.state.step === this.state.squares.length){
-                headerState = `Its a draw!`;
-            }else{
-                headerState = `Its your turn: ${player}`;
-            }
-           
-        }
-        return(
-            <div className ="board-wrapper">
-                <div className="board-header">{headerState}</div>
-                <div className="board-body">
-
-                    <div className="row">
-                        {this.drawSquare(0)}
-                        {this.drawSquare(1)}
-                        {this.drawSquare(2)}
-                    </div>
-                    <div className="row">
-                        {this.drawSquare(3)}
-                        {this.drawSquare(4)}
-                        {this.drawSquare(5)}
-                    </div>
-                    <div className="row">
-                        {this.drawSquare(6)}
-                        {this.drawSquare(7)}
-                        {this.drawSquare(8)}
-                    </div>
-
-                </div>
-            </div>
-        )
-    }
+    return (
+      <div className="board-wrapper">
+        <div className="board-header">{headerState}</div>
+        <div className="board-body">
+          <div className="row">
+            {this.drawSquare(0)}
+            {this.drawSquare(1)}
+            {this.drawSquare(2)}
+          </div>
+          <div className="row">
+            {this.drawSquare(3)}
+            {this.drawSquare(4)}
+            {this.drawSquare(5)}
+          </div>
+          <div className="row">
+            {this.drawSquare(6)}
+            {this.drawSquare(7)}
+            {this.drawSquare(8)}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default Board;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,21 +1,42 @@
 import React from "react";
-const btnStyle = {
-        background: "#fff",
-        border: '1px solid #999',
-        float: 'left',
-        fontSize: '24px',
-        fontWeight: 'bold',
-        lineHeight: '34px',
-        height: '34px',
-        marginRight: '-1px',
-        marginTop: '-1px',
-        padding: 0,
-        textAlign: 'center',
-        width: '34px'
-           
-}
-export const Square = ({handleClick,value}) => (
-    <button style={btnStyle} onClick = {handleClick}>
-        {value}
-    </button>
-)
+import { withStyles } from "@material-ui/core/styles";
+import Icon from "@material-ui/core/Icon";
+
+const iconStyle = {
+  background: "#fff",
+  border: "1px solid #999",
+  float: "left",
+  fontSize: "6em",
+  fontWeight: "bold",
+  height: "100",
+  marginRight: "-1px",
+  marginTop: "-1px",
+  padding: 0,
+  textAlign: "center",
+  width: "100"
+};
+
+const styles = theme => ({
+  iconStyle: iconStyle
+});
+/**
+ * Render the proper Icon X or O
+ * @param {String} playerTurn
+ */
+const squareIcon = playerTurn => {
+  switch (playerTurn) {
+    case "X":
+      return "clear";
+    case "O":
+      return "panorama_fish_eye";
+    default:
+      return "";
+  }
+};
+
+const Square = ({ handleClick, value, classes }) => (
+  <Icon className={classes.iconStyle} onClick={handleClick}>
+    {squareIcon(value)}
+  </Icon>
+);
+export default withStyles(styles)(Square);
